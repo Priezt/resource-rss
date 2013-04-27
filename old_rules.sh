36f104ae62b7bc7be6a6d7f38d97145ee65cd3ca
Priezt <vegetabird@gmail.com>
2013-04-27 02:37:30 +0000

    nfc

--- old_rules.sh
+++ old_rules.sh
@@ -34,42 +34,10 @@ yyets http://www.yyets.com/php/resource/27274 BeautyAndTheBeast
 yyets http://www.yyets.com/php/resource/11049 AmericanHorrorStory
 yyets http://www.yyets.com/php/resource/11030 Boardwalk
 
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/26753 AvengersS02
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/26745 Tron
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/26737 UltimateSpiderMan
-yyets http://www.yyets.com/php/resource/29024 UltraQ
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/10910 SuitsS02
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11013 Fringe
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11037 Merlin
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11015 Supernatural
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11005 TBBT
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/26779 Arrow
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11057 TheWalkingDead
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11007 Mentalist
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/26195 Dexter
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/10990 TwoAndAHalfMen
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11176 Spartacus
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/28793 HouseOfCards
-yyets http://www.yyets.com/php/resource/29134 NeoUltraQ
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/11103 2BrokeGirls
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/28235 BlackMirror
-yyets http://www.yyets.com/php/resource/10452 Archer
-yyets http://www.yyets.com/php/resource/29316 GreenLantern
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/29311 Vikings
-yyets_rmvb_mp4 http://www.yyets.com/php/resource/29326 Bible
-yyets_rmvb_mp4 http://www.yyets.com/resource/10733 GameOfThrones
-yyets_rmvb_mp4 http://www.yyets.com/resource/28404 DaVincisDemons
-yyets_rmvb_mp4 http://www.yyets.com/resource/28764 Hannibal
-yyets_rmvb_mp4 http://www.yyets.com/resource/29725 Galileo
-yyets_rmvb_mp4 http://www.yyets.com/resource/29676 Defiance
-yyets http://www.yyets.com/resource/26326 DoctorWho
-yyets_rmvb_mp4 http://www.yyets.com/resource/29752 MinnaEsupaDayo
 
 pq-rip.py -u 'http://www.yyets.com/' 'div.top24hours ul li a' | $FMR YYeTsHome
 
 #yyets() { get-rendered-url.js $1 | pq-rip.py -u -s $1 'ul.resod_list li span.l a span.a' | $FMR $2 ; }
 #yyets_rmvb_mp4() { get-rendered-url.js $1 | pq-rip.py -u -s $1 'ul.resod_list li span.l a span.a' | egrep -i 'rmvb|mp4' | $FMR $2 ; }
-yyets() { w3m -dump -cols 500  $1 | grep '[ ]' | egrep 'mkv|rmvb|avi|mp4' | perl -pe 's/.* (1080P|720P|BD-720P|DVD|DVDSCR|HDTV|HR-HDTV|MP4|RMVB|WEB-DL)//' | perl -pe 's/(\.(?:mkv|rmvb|avi|mp4)).*/$1/' | perl -pe '$_ = "'$1'|".$_' | $FMR $2 ; }
-yyets_rmvb_mp4() { w3m -dump -cols 500  $1 | grep '[ ]' | egrep 'rmvb|mp4' | perl -pe 's/.* (1080P|720P|BD-720P|DVD|DVDSCR|HDTV|HR-HDTV|MP4|RMVB|WEB-DL)//' | perl -pe 's/(\.(?:mkv|rmvb|avi|mp4)).*/$1/' | perl -pe '$_ = "'$1'|".$_' | $FMR $2 ; }
 
 pq-rip.py -u 'http://dzyey.com/about.php?class_id=10&page=1' 'table td[width="68%"] > a' | $FMR DongZhan
--- update.sh
+++ update.sh
@@ -10,7 +10,42 @@ FMR=/home/priezt/code/resource-rss/fresh-make-rss.sh
 # yayaxz
 yayaxz() { curl -s $1 | grep -A 5 resource-item | grep '<a href' | grep -v 'class="type"' | perl -pe 's/.*?>//;s/<.*//;' | perl -pe '$_ = "'$1'|".$_' | $FMR $2; }
 
-yayaxz http://www.yayaxz.com/resource/10733 GameOfThrones
+#yayaxz http://www.yayaxz.com/resource/10733 GameOfThrones
+
+# yyets
+yyets() { w3m -dump -cols 500  $1 | grep '[ ]' | egrep 'mkv|rmvb|avi|mp4' | perl -pe 's/.* (1080P|720P|BD-720P|DVD|DVDSCR|HDTV|HR-HDTV|MP4|RMVB|WEB-DL)//' | perl -pe 's/(\.(?:mkv|rmvb|avi|mp4)).*/$1/' | perl -pe '$_ = "'$1'|".$_' | $FMR $2 ; }
+yyets_rmvb_mp4() { w3m -dump -cols 500  $1 | grep '[ ]' | egrep 'rmvb|mp4' | perl -pe 's/.* (1080P|720P|BD-720P|DVD|DVDSCR|HDTV|HR-HDTV|MP4|RMVB|WEB-DL)//' | perl -pe 's/(\.(?:mkv|rmvb|avi|mp4)).*/$1/' | perl -pe '$_ = "'$1'|".$_' | $FMR $2 ; }
+
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/26753 AvengersS02
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/26745 Tron
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/26737 UltimateSpiderMan
+yyets http://www.yyets.com/php/resource/29024 UltraQ
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/10910 SuitsS02
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11013 Fringe
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11037 Merlin
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11015 Supernatural
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11005 TBBT
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/26779 Arrow
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11057 TheWalkingDead
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11007 Mentalist
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/26195 Dexter
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/10990 TwoAndAHalfMen
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11176 Spartacus
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/28793 HouseOfCards
+yyets http://www.yyets.com/php/resource/29134 NeoUltraQ
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/11103 2BrokeGirls
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/28235 BlackMirror
+yyets http://www.yyets.com/php/resource/10452 Archer
+yyets http://www.yyets.com/php/resource/29316 GreenLantern
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/29311 Vikings
+yyets_rmvb_mp4 http://www.yyets.com/php/resource/29326 Bible
+yyets_rmvb_mp4 http://www.yyets.com/resource/10733 GameOfThrones
+yyets_rmvb_mp4 http://www.yyets.com/resource/28404 DaVincisDemons
+yyets_rmvb_mp4 http://www.yyets.com/resource/28764 Hannibal
+yyets_rmvb_mp4 http://www.yyets.com/resource/29725 Galileo
+yyets_rmvb_mp4 http://www.yyets.com/resource/29676 Defiance
+yyets http://www.yyets.com/resource/26326 DoctorWho
+yyets_rmvb_mp4 http://www.yyets.com/resource/29752 MinnaEsupaDayo
 
 # comic
 pq-rip.py -u 'http://www.dm5.com/manhua-huofengliaoyuan/' '#cbc_1 > li > a' | grep -v javascript | $FMR HuoFeng
