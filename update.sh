68b99b3eadc4b41e0ad4b09d3362f94dc02b22e8
Priezt <vegetabird@gmail.com>
2013-08-01 14:46:48 +0000

    nfc

--- update.sh
+++ update.sh
@@ -52,6 +52,7 @@ yyets_rmvb_mp4 http://www.yyets.com/resource/30010 AvengersAssemble
 yyets_rmvb_mp4 http://www.yyets.com/resource/30025 UnderTheDome
 yyets_rmvb_mp4 http://www.yyets.com/resource/30347 BewareTheBatman
 yyets_rmvb_mp4 http://www.yyets.com/resource/30369 Akuryo
+yyets_rmvb_mp4 http://www.yyets.com/resource/30438 SenzyoTanteiKageyama
 
 
 pq-rip.py -u 'http://www.yyets.com/' 'div.top24hours ul li a' | $FMR YYeTsHome
