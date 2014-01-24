34e472dca6e64211ebb826a9f6ee1af5340de153
Priezt <vegetabird@gmail.com>
2014-01-25 00:30:34 +0800

    nfc

---
+++ rip-playno1.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'nokogiri'
+
+page = Nokogiri::HTML(open('http://www.playno1.com/portal.php?mod=list&catid=4'))
+page.css("div.fire_float h3 a").each do |a|
+	puts "#{a.text}|http://www.playno1.com/#{a.attr("href")}"
+end
