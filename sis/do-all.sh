b47289baa872ab07d9ae81a347f135d6904b6d05
Priezt <vegetabird@gmail.com>
2014-06-22 11:02:15 -0400

    merge pages

--- sis/do-all.sh
+++ sis/do-all.sh
@@ -7,3 +7,5 @@ cat ,threads | fresh.pl sis > ,new-threads
 wc -l ,new-threads
 echo "Get each thread"
 cat ,new-threads | xargs -I , ./get-one-thread.sh ,
+
+ls threads/ | grep -v dummy | sort -r | head -100 | xargs -I , find threads/, -name index.html | xargs -I , cat , > index.html
