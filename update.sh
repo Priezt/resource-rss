7da86cd0f15bb1dbf43fefd0f025bf579a5bfc52
Priezt <vegetabird@gmail.com>
2014-06-22 01:09:29 -0400

    nfc

--- update.sh
+++ update.sh
@@ -66,6 +66,7 @@ yyets_rmvb_mp4 http://www.yyets.com/resource/31790 MindGames
 yyets_rmvb_mp4 http://www.yyets.com/resource/31998 DiskWars
 yyets_rmvb_mp4 http://www.yyets.com/resource/32018 Salem
 yyets_rmvb_mp4 http://www.yyets.com/resource/32235 TheFlash
+yyets_rmvb_mp4 http://www.yyets.com/resource/32281 Dominion
 
 #yayaxz http://www.yayaxz.com/resource/26753 yayaAvengersS02
 #yayaxz http://www.yayaxz.com/resource/26745 yayaTron
