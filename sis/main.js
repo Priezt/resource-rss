bc8d0c2a091ec6a43421641cad14465141755c24
Priezt <vegetabird@gmail.com>
2014-07-02 11:14:12 -0400

    add key shortcut

--- sis/main.js
+++ sis/main.js
@@ -2,6 +2,13 @@ $(init);
 
 function init(){
 	$("div.close").click(function(){
+		window.scrollTo(0, 0);
 		$(this).parent().remove();
+	});
+	$("body").keypress(function(event){
+		console.log(event);
+		if(event.which == 100){// d
+			$("div.close").first().click();
+		}
 	});
 }
