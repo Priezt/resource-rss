dbc1086aa9268f75cd1523db63846f8c8dc8297a
Priezt <vegetabird@gmail.com>
2014-07-05 03:36:54 -0400

    nfc

--- update.sh
+++ update.sh
@@ -185,6 +185,8 @@ j.rip 'http://dv.56.com/funny/?do=review' 'div.review_list li' 'div.vid_info' | 
 
 j.rip http://bt.ktxp.com/team-222-1.html 'td.ttitle' 'a:eq(2)' 'a:eq(2)' | ./jrrs.sh KRL
 
+j.rip 'http://bt.ktxp.com/search.php?keyword=JOJO%E5%A5%87%E5%A6%99%E5%86%92%E9%99%A9%E5%90%A7' 'td.ttitle' 'a:eq(2)' 'a:eq(2)' | ./jrrs.sh JoJo3KTXP
+
 j.rip http://www.chinaocg.cn/ 'div.items' 'a' | ./jrrs.sh CHINAOCG
 
 date
