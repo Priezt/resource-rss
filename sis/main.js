ba7be2c2bf8c49b019df6a620e78c8bba282c1f5
Priezt <vegetabird@gmail.com>
2014-07-05 12:09:54 -0400

    add notification for sis

--- sis/main.js
+++ sis/main.js
@@ -1,14 +1,37 @@
+var killed = [];
+var libnotify = humane.create({baseCls: 'humane-libnotify', addnCls: 'humane-libnotify-info'})
 $(init);
 
+function show_div_count(){
+	humane.remove(function(){});
+	libnotify.log($("div.thread").size());
+}
+
+function delete_div(){
+	window.scrollTo(0, 0);
+	killed.push($(this).parent());
+	if(killed.length > 10){
+		killed.shift();
+	}
+	console.log(killed);
+	$(this).parent().remove();
+	show_div_count();
+}
+
 function init(){
-	$("div.close").click(function(){
-		window.scrollTo(0, 0);
-		$(this).parent().remove();
-	});
+	$("div.close").click(delete_div);
 	$("body").keypress(function(event){
-		console.log(event);
+		//console.log(event);
 		if(event.which == 100){// d
 			$("div.close").first().click();
+		}else if(event.which == 114){// r
+			if(killed.length > 0){
+				var last_div = killed.pop();
+				last_div.insertBefore($("div.close").first().parent());
+				last_div.find("div.close").click(delete_div);
+				console.log(killed);
+				show_div_count();
+			}
 		}
 	});
 }
