617630319caadb308d9ff08e456d2d6f7a2af09a
Priezt <vegetabird@gmail.com>
2015-07-01 20:05:46 -0400

    nfc

--- update.sh
+++ update.sh
@@ -168,7 +168,8 @@ xunbo() { pq-rip.py http://www.4567.tv/film/id$1.html 'p.name' | perl -pe '$_ = 
 xunbo 20296 GameOfThrone
 xunbo 20663 SiliconValley
 
-magdown() { j.rip 'http://cili002.com/?topic_title3='"$1" 'span.b' 'a' 'a' | ./jrrs.sh magdown_$2 ; }
+#magdown() { j.rip 'http://cili002.com/?topic_title3='"$1" 'span.b' 'a' 'a' | ./jrrs.sh magdown_$2 ; }
+magdown() { j.rip 'http://cili003.com/?topic_title3='"$1" 'span.b' 'a' 'a' | ./jrrs.sh magdown_$2 ; }
 
 magdown '%E6%9D%83%E5%8A%9B%E7%9A%84%E6%B8%B8%E6%88%8F' GameOfThrones
 magdown '%E7%A1%85%E8%B0%B7' SiliconValley
