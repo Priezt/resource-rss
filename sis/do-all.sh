b95acfcd36d2aa9c21f7d615fa49ccfb7c3ceb76
Priezt <vegetabird@gmail.com>
2015-07-06 08:01:43 -0400

    nfc

--- sis/do-all.sh
+++ sis/do-all.sh
@@ -12,3 +12,8 @@ cat ,new-threads | xargs -I , ./get-one-thread.sh ,
 echo "Merge index.html"
 ls threads/ | grep -v dummy | sort -r | head -100 | xargs -I , find threads/, -name index.html | xargs -I , cat , > ,index-content.html
 cat index-header.html ,index-content.html index-tail.html > index.html
+
+cd /home/priezt/code/resource-rss/
+tar zcf sis.tar.gz sis
+cp sis.tar.gz ~/OneDrive/Temp/sis.tar.gz
+
