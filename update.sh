07e7bbe8ef783b40e4f589a8a0eb0e54d47af2d9
Priezt <vegetabird@gmail.com>
2015-07-12 21:07:52 -0400

    nfc

--- update.sh
+++ update.sh
@@ -175,6 +175,7 @@ magdown '%E6%9D%83%E5%8A%9B%E7%9A%84%E6%B8%B8%E6%88%8F' GameOfThrones
 magdown '%E7%A1%85%E8%B0%B7' SiliconValley
 magdown '%E9%BB%91%E5%AE%A2%E5%86%9B%E5%9B%A2' MrRobot
 magdown '%E7%9C%9F%E5%AE%9E%E7%9A%84%E4%BA%BA%E7%B1%BB' Humans
+magdown 'Extant' Extant
 
 webtoon() { j.rip 'http://www.webtoons.com/zh-hans/thriller/'"$1" 'ul#_listUl li' 'span.subj' 'a' 'span.thmb img' | ./jrrs.sh webtoon_$2 ; }
 
